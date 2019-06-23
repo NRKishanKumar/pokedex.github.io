eff8405c8111c8acf34f27e514364f8255da2fbb
Kishan Kumar <kishan.kumar@intimetec.com>
2019-06-23 23:36:01 +0530

    update2

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -6,7 +6,7 @@ class NavBar extends Component {
             <div>
                 <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
                     <a className="navbar-brand col-sm-3 col-md-2 mr-0 align-items-center" href={'#'}>
-                        Pokemon list
+                        Pokemon data
                     </a>
                 </nav>
             </div>
