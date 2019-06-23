c40f75ad9cacec8f30728cdafcb144f7f7e1aae4
Kishan Kumar <kishan.kumar@intimetec.com>
2019-06-23 23:07:38 +0530

    Updates

---
+++ precache-manifest.dd90bb6854235fe7d516f560922aeeb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0628b1a7910e61dd866cc0b39b2181ba",
+    "url": "/pokedex.github.io/index.html"
+  },
+  {
+    "revision": "8ea51b65c9e6d4a337ba",
+    "url": "/pokedex.github.io/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "f5136ca6040b44fb6db7",
+    "url": "/pokedex.github.io/static/css/main.132a93b0.chunk.css"
+  },
+  {
+    "revision": "8ea51b65c9e6d4a337ba",
+    "url": "/pokedex.github.io/static/js/2.92d2e8f7.chunk.js"
+  },
+  {
+    "revision": "f5136ca6040b44fb6db7",
+    "url": "/pokedex.github.io/static/js/main.22ef2751.chunk.js"
+  },
+  {
+    "revision": "54f5c626aaea864b922c",
+    "url": "/pokedex.github.io/static/js/runtime~main.f7bc5cf3.js"
+  },
+  {
+    "revision": "964c12ef9060e0555f4901420fc6473d",
+    "url": "/pokedex.github.io/static/media/pattern.964c12ef.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/NRKishanKumar/pokedex.github.io/precache-manifest.7e9403eecf5c728201e92ee1972a56f3.js"
+  "/pokedex.github.io/precache-manifest.dd90bb6854235fe7d516f560922aeeb2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/NRKishanKumar/pokedex.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pokedex.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
