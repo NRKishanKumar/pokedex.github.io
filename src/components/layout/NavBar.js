3ea55162eb701b2a583110d1ffed298c1a400eef
Kishan Kumar <kishan.kumar@intimetec.com>
2019-06-23 23:34:05 +0530

    update1

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,7 +1,4 @@
 import React, {Component} from 'react';
-import styled from 'styled-components';
-
-// const NavBarStyle = styled.nav;
 
 class NavBar extends Component {
     render() {
@@ -9,7 +6,7 @@ class NavBar extends Component {
             <div>
                 <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
                     <a className="navbar-brand col-sm-3 col-md-2 mr-0 align-items-center" href={'#'}>
-                        Pokedex
+                        Pokemon list
                     </a>
                 </nav>
             </div>
